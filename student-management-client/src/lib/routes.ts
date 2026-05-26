c74611272a3aae9a8001aa5ad2839095bb3562b8
trongseag <101387957+Trongsangvu@users.noreply.github.com>
2026-05-26 11:38:37 +0700

    update routes

--- student-management-client/src/lib/routes.ts
+++ student-management-client/src/lib/routes.ts
@@ -1,18 +1,15 @@
 export const routes = {
   home: '/',
+
   students: '/students',
-  addStudent: '/students/add',
   studentDetail: (id: string) => `/students/${id}`,
 
   subjects: '/subjects',
-  addSubject: '/subjects/add',
   subjectDetail: (id: string) => `/subjects/${id}`,
 
   classes: '/classes',
-  addClass: '/classes/add',
   classDetail: (id: string) => `/classes/${id}`,
 
   scores: '/scores',
-  addScore: '/scores/add',
   scoreDetail: (id: string) => `/scores/${id}`,
 };
